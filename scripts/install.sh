ea277a3d4455009c585f14b4ae7af05b2a4e9dda
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2023-06-07 15:48:29 +0530

    Revert "add support for arm64 compile (#88)"
    This reverts commit a6a3130223f99786ee0a976cc0e02be4b89eb0cb.

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,9 +4,6 @@ set -e
 
 VERSION="v1.3.0"
 
-gitDir=$(realpath `dirname $BASH_SOURCE`/..)
-echo $gitDir
-
 # uname -s, uname -m
 # Deb 32: Linux i686
 # Ubuntu 64: Linux x86_64
@@ -15,21 +12,6 @@ echo $gitDir
 if [[ "$UID" != 0 ]]; then
     echo NOTE: sudo needed to set up and run start service
     exit 1
-fi
-
-
-if [[ ! "`which git 2> /dev/null`" == "" ]]; then
-    thisGit=`git -C "${gitDir}" config --get remote.origin.url`
-    thisGit=${thisGit::-4}
-    GITHUB_BINARY_BASE="${thisGit}/releases/download"
-    GITHUB_RAW_BASE="${thisGit/github.com/raw.githubusercontent.com}/releases/download"
-fi
-
-if [[ $thisGit == "" ]]; then
-    GITHUB_URL_PARTS="MatchbookLab/local-persist"
-    GITHUB_BINARY_BASE="https://github.com/${GITHUB_URL_PARTS}/releases/download"
-    GITHUB_RAW_BASE="https://raw.githubusercontent.com/${GITHUB_URL_PARTS}/"
-    GITHUB_URL_PARTS=
 fi
 
 
@@ -42,10 +24,6 @@ function setenv {
         case $ARCH in
             "x86_64")
                 ARCH="amd64"
-                SUPPORTED=true
-            ;;
-            "aarch64")
-                ARCH="arm64"
                 SUPPORTED=true
             ;;
             "i686")
@@ -78,7 +56,7 @@ function install-binary {
         (sudo systemctl stop docker-volume-local-persist || true)
     fi
 
-    BINARY_URL="${GITHUB_BINARY_BASE}/${VERSION}/local-persist-${OS}-${ARCH}"
+    BINARY_URL="https://github.com/CWSpear/local-persist/releases/download/${VERSION}/local-persist-${OS}-${ARCH}"
     BINARY_DEST="/usr/bin/docker-volume-local-persist"
 
     echo Downloading binary:
@@ -94,7 +72,7 @@ function install-binary {
 
 # Systemd (default)
 function setup-systemd {
-    SYSTEMD_CONFIG_URL="${GITHUB_RAW_BASE}/${VERSION}/init/systemd.service"
+    SYSTEMD_CONFIG_URL="https://raw.githubusercontent.com/CWSpear/local-persist/${VERSION}/init/systemd.service"
     SYSTEMD_CONFIG_DEST="/etc/systemd/system/docker-volume-local-persist.service"
 
     echo Downloading Systemd service conf:
@@ -121,7 +99,7 @@ function start-systemd {
 
 # Upstart
 function setup-upstart {
-    UPSTART_CONFIG_URL="${GITHUB_RAW_BASE}/${VERSION}/init/upstart.conf"
+    UPSTART_CONFIG_URL="https://raw.githubusercontent.com/CWSpear/local-persist/${VERSION}/init/upstart.conf"
     UPSTART_CONFIG_DEST="/etc/init/docker-volume-local-persist.conf"
 
     echo Downloading binary:
